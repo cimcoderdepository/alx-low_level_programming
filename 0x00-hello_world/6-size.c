417b9cbe81e5988689914904eb354cd1b02efa8a
cimcoderdepository <gloryland4ever@gmail.com>
2022-12-09 11:38:14 +0100

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,11 +13,11 @@ int main(void)
 	long long int d;
 	float f;
 
-	printf("size of a char: %ld byte\n", sizeof(a));
-	printf("size of an int: %ld bytes\n", sizeof(b));
-	printf("size of a long int: %ld bytes\n", sizeof(c));
-	printf("size of a long long int: %ld bytes\n", sizeof(d));
-	printf("size of a float: %ld bytes\n", sizeof(f));
-	fprintf("stderr\n", [Anything]);
+	printf('Size of a char: %lu byte(s)\n', (unsigned long)sizeof(a));
+	printf('Size of an int: %lu byte(s)\n', (unsigned long)sizeof(b));
+	printf('Size of a long int: %lu byte(s)\n', (unsigned long)sizeof(c));
+	printf('Size of a long long int: %lu byte(s)\n', (unsigned long)sizeof(d));
+	printf('Size of a float: %lu byte(s)\n', (unsigned long)sizeof(f));
+	fprintf('Stderr\n', '[Anything]');
 	return (0);
 }
