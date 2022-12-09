162ecc08151de4aaefd4f6c7942717194878a91d
cimcoderdepository <gloryland4ever@gmail.com>
2022-12-09 04:54:04 +0100

    task 6 fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,12 @@
  */
 int main(void)
 {
-	"char a\n";
-	"int b\n";
-	"long int c\n";
-	"long long int d\n";
-	"float f\n";
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
 	printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
