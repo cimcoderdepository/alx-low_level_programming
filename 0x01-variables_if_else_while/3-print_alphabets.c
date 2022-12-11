712495bec0c5740d7b6f3721134a526d47c964e0
cimcoderdepository <gloryland4ever@gmail.com>
2022-12-11 04:39:54 +0100

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Success at 0
+ */
+int main(void)
+{
+	char ch = 97;
+
+	for (ch = 97; ch < 123; ch++)
+	{
+		putchar(ch);
+	}
+		putchar('\n');
+
+	for (ch = 65; ch < 90; ch++)
+	{
+		putchar(ch);
+	}
+		putchar('\n');
+		return (0);
+}
