b1a593029c3a1f38b251b83fc4466403cc9c4ebc
cimcoderdepository <gloryland4ever@gmail.com>
2022-12-11 18:53:42 +0100

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Success at 0
+ */
+int main(void)
+{
+	char ch = 122;
+
+	for (ch = 122; ch > 96; --ch)
+	{
+		putchar(ch);
+	}
+		putchar('\n');
+		return (0);
+}
