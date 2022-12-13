b93a3c7edd85616fcd897eaff64c71c19bcd422e
cimcoderdepository <gloryland4ever@gmail.com>
2022-12-13 21:34:15 +0100

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
 
-void display()
+/**
+ * display - funtion to print _putchar.
+ *
+ * Description:  user-defined function.
+ */
+void display(void)
 {
+
 	printf("_putchar\n");
+
 }
+
+/**
+ * main - This is the preferred style for multi-line.
+ *
+ * Return:  returns 0 on Success.
+ */
 int main(void)
 {
+
 	display();
 
 	return (0);
