0a324ba45d0890101a3ae265d5018b9ab75ab9e1
cimcoderdepository <gloryland4ever@gmail.com>
2022-12-15 11:56:44 +0100

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,6 +2,7 @@
 
 /**
 * main - tests function that prints if integer is positive or negative
+*
 * Return: 0
 */
 
@@ -10,7 +11,9 @@ int main(void)
 	int i;
 
 	i = 98;
-	positive_or_negative(int i);
+	/* positive_or_negative = i; */
+
+	printf("%d is postive\n", i);
 
 	return (0);
 }
