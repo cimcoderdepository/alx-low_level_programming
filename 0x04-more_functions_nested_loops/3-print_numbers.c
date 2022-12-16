65ebf4353d4179f0228b64e25b767bb1919b17b6
cimcoderdepository <gloryland4ever@gmail.com>
2022-12-17 00:25:38 +0100

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - A function that prints numbers from 0-9.
+ */
+
+void print_numbers(void)
+{
+	int c = 0;
+
+	for (c = 48; c <= 57; c++)
+	_putchar(c);
+	_putchar('\n');
+}
