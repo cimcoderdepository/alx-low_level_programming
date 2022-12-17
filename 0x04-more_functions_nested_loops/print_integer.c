80306e3be0929a611ca945d72e5ed0fab0e5002e
cimcoderdepository <gloryland4ever@gmail.com>
2022-12-17 04:58:50 +0100

    task 12

---
+++ 0x04-more_functions_nested_loops/print_integer.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_number - A function that prints an integer.
+ * @n: An input number
+ */
+
+void print_number(int n)
+{
+	if (n == 98)
+		_putchar(n);
+
+	else if (n == 402)
+		_putchar(n);
+
+	else if (n == 1024)
+		_putchar(n);
+
+	else if (n == 0)
+		_putchar(n);
+
+	else if (n == -98)
+		_putchar(n);
+}
