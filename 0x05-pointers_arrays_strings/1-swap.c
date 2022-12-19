5db8ce7572c9453d0c04befd8490af16ee5f2dde
cimcoderdepository <gloryland4ever@gmail.com>
2022-12-19 13:38:07 +0100

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - A function that takes a pointer to
+ * an int as parameter and updates the value
+ * it points to to 98
+ *
+ * @a: value to be swapped
+ * @b: value to be swapped
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
