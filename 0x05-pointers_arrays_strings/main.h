6b68ed226f07832dc45957b0707a285136f03baf
cimcoderdepository <gloryland4ever@gmail.com>
2022-12-21 00:34:27 +0100

    task 3

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,8 +2,12 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <string.h>
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
+int _putchar(char c);
+void _puts(char *str);
 
 #endif
