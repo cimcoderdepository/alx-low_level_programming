f1ab632dfb867f12e7b78e2e2f27d78ec0acac8a
cimcoderdepository <gloryland4ever@gmail.com>
2022-12-21 01:41:41 +0100

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _puts - A function that prints a string
+ * @str: character to be printed
+*/
+
+void print_rev(char *s)
+{
+	int i, length = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		length++;
+
+	}
+	for (i = 0; i <= length; i++)
+	{
+
+		_putchar(*s--);
+	}
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+void rev_string(char *s)
+{
+	while (*s == '\0')
+	l = ('\0' - 1);
+	_putchar(l);
+
+	
