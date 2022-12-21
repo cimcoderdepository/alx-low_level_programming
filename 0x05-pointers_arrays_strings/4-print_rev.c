82c357d0ee35729f69e7609044d31a8c38a3a1c7
cimcoderdepository <gloryland4ever@gmail.com>
2022-12-21 02:09:56 +0100

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,26 +1,20 @@
 #include "main.h"
 
 /**
- * _puts - A function that prints a string
- * @str: character to be printed
-*/
-
+ * print_rev - a function that prints a string, in reverse,
+ * followed by a new line.
+ * @s: an input string
+ * Return: Nothing
+ */
 void print_rev(char *s)
 {
-	int i, length = 0;
+	int len = 0;
 
-	while (*s != '\0')
-	{
-		s++;
-		length++;
+	while (s[len] != '\0')
+		len++;
 
-	}
-		/* s = s - 1; */
+	while (len)
+		_putchar(s[--len]);
 
-	for (i = 0; i <= length; i++)
-	{
-
-		_putchar(*--s);
-	}
 	_putchar('\n');
 }
