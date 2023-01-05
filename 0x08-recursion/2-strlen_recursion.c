f5fe67b371e256f8c62a54c3ed3de4baf21ed0c0
cimcoderdepository <gloryland4ever@gmail.com>
2023-01-05 05:29:28 +0100

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *  _strlen_recursion - s function that returns the length of a string
+ * @s: An input string to print
+ * Returns: returns the length of the string
+ */
+int _strlen_recursion(char *s)
+{
+
+	if (*s)
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	return (0);
+}
+			
