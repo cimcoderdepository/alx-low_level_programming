df5b8a36b82e2c89301cb8956cdeca6d478772a9
cimcoderdepository <gloryland4ever@gmail.com>
2023-01-07 11:19:59 +0100

    modify all my *.c contents to generate *.o, STATIC_LIBRARY (.a) FILE, quote (.exe) file

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,19 +1,15 @@
 #include "main.h"
 
 /**
- * _puts - Write a function that prints a string, followed
- * by a new line, to stdout.
- * @str: an input string
- * Return: Nothing
- */
-void _puts(char *s)
-{
-	int i = 0;
+ * _puts - A function that prints a string
+ * @str: character to be printed
+*/
 
-	while (s[i] != '\0')
+void _puts(char *str)
+{
+	while (*str != '\0')
 	{
-		_putchar(s[i]);
-		i++;
+		_putchar(*str++);
 	}
 	_putchar('\n');
 }
