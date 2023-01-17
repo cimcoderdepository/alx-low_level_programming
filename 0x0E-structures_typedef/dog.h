f79e2dd13f9b3e435eeb7fac61f4d1de249f2ffc
cimcoderdepository <gloryland4ever@gmail.com>
2023-01-17 19:24:30 +0100

    use betty to correct checker-error

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef dog_h
 #define dog_h
-
+/**
+ * struct dog - a new type "struct dog" with the following elements:
+ * @name: name input
+ * @age: age input
+ * @owner: owner input
+*/
 struct dog
 {
 	char *name;
