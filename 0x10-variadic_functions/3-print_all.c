3f8cc525db8d2d09130ab65ec97329f0958848df
cimcoderdepository <gloryland4ever@gmail.com>
2023-01-21 20:17:01 +0100

    task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,83 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_char - a function to print a character.
+ * @arg: a character input passed
+*/
+void print_char(va_list arg)
+{
+	printf("%c", va_arg(arg, int));
+}
+/**
+* print_int - a function to print an integer.
+* @arg: an integer input passed
+*/
+void print_int(va_list arg)
+{
+	 printf("%i", va_arg(arg, int));
+}
+/**
+* print_float - a function to print a float.
+* @arg: a float input passed
+*/
+void print_float(va_list arg)
+{
+	printf("%f", va_arg(arg, double));
+}
+/**
+* print_str - a function to print a string.
+* @arg: a string input passed
+*/
+void print_str(va_list arg)
+{
+	char *string = va_arg(arg, char *);	/* we create a buffer...*/
+	/*...(a.k.a container) called [string] to hold [va_arg(arg, char *)] */
+
+	if (string)	/* if string is contains something...*/
+			/*...Meaning it is NOT NULL (empty) */
+
+		printf("%s", string);
+
+	else	/* if the string is NULL */
+		printf("(nil)");
+}
+
+/**
+* print_all - a function that prints anything.
+* @format: where format is a list of types of arguments passed to the function
+*/
+void print_all(const char * const format, ...)
+{
+	va_list uduak;
+	int i = 0, j;	/* [i] to be looping through "ceis" in the...*/
+			/*...main file. see (3-main.c) file for more info */
+
+	prt format_funcs[] = {	/* [j] to be looping through the structure */
+	/*[ptr format_funcs[]. we initailize "ceis" array to access its index */
+
+		{'c', print_char},	/* format_funcs[j][0] - 0th index */
+		{'i', print_int},	/* format_funcs[j][1] - 1st index */
+		{'f', print_float},	/* format_funcs[j][2] - 2nd index */
+		{'s', print_str}	/* format_funcs[j][3] - 3rd index */
+	};
+
+	va_start(uduak, format);
+
+	while (format[i])
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (format[i] == format_funcs[j].symbol)
+			{
+				format_funcs[j].print(uduak);
+				printf(", ");
+			}
+		}
+		i++;
+	}
+	format_funcs[j].print(uduak);
+	printf("\n");
+	va_end(uduak);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,47 @@
 #ifndef variadic_functions_h
 #define variadic_functions_h
 
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * struct print_format - a struct to combine known data types
+ * @symbol: a array of character
+ * @print: a function pointer.
+ * prt - typedef for struct print_format.
+ */
+typedef struct print_format
+{
+	char symbol;
+	void (*print)(va_list arg);
+} prt;
+
+	/* for the above typedef, we have to */
+	/*...create functions for char, integer, */
+	/*...float, char *; and put their prototype */
+	/*...in our  header file */
+
+/**
+ * print_char - a function to print a character.
+ * @arg: a character input passed
+*/
+void print_char(va_list arg);
+/**
+* print_int - a function to print an integer.
+* @arg: an integer input passed
+*/
+void print_int(va_list arg);
+/**
+* print_float - a function to print a float.
+* @arg: a float input passed
+*/
+void print_float(va_list arg);
+/**
+* print_str - a function to print a string.
+* @arg: a string input passed
+*/
+void print_str(va_list arg);	/*...in our  header file */
+
+
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
