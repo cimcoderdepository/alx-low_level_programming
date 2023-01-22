09b2eb6e83a1cdd186fcaa37ee7a105b31750b07
cimcoderdepository <gloryland4ever@gmail.com>
2023-01-22 20:43:24 +0100

    task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,25 +10,25 @@
 
 int main(int argc, char **argv)
 {
-	int num1 = atoi (argv[1]);
-	int num2 = atoi (argv[3]);
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[3]);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
-		exit (98);
+		exit(98);
 	}
 	if (!get_op_func(argv[2]))
 	{
 		printf("Error\n");
-		exit (99);
+		exit(99);
 	}
-	if ((*(argv[2]) == '/') || (*(argv[2]) == '%'))
+	if (*argv[2] == '/' || *argv[2] == '%')
 	{
 			if (num2 == 0)
 			{
 				printf("Error\n");
-				exit (100);
+				exit(100);
 			}
 	}
 	printf("%d\n", (get_op_func)(argv[2])(num1, num2));
