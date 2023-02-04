8601d9a6e7e585c61324e5739bc7c88cdd691b7d
cimcoderdepository <gloryland4ever@gmail.com>
2023-02-04 12:08:37 +0100

    cant find this directory in github

---
+++ 0x14-bit_manipulation/pow.c
@@ -0,0 +1,24 @@
+#include <math.h>
+#include <stdio.h>
+int main(void) {
+	int a = 0;
+	double result = 3 * pow(4, 5);
+
+	//printf("enter exponent number: ");
+	//scanf("%d", &a);
+	printf("\n3 * %d^%d is = %lf\n", 4, 5, result);
+
+
+	return 0;
+}
+/*
+#include <math.h>
+#include <stdio.h>
+
+int main(void)
+{
+    double result = pow(4, 3);
+    printf("4 raised to the power of 3 is %lf\n", result);
+    return 0;
+}
+*/
