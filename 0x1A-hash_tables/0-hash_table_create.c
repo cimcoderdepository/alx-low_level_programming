96c800814c85f83fdb1675dcc90a0698f10d06c7
cimcoderdepository <gloryland4ever@gmail.com>
2023-04-07 20:05:12 +0100

    task 2

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -18,14 +18,15 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t *) * size);
+	ht->array = malloc(sizeof(hash_node_t **) * size);
 	if (ht->array == NULL)
 	{
 		return NULL;
 	}
-	for (; i < size; i++)
+	while (i < size)
 	{
 		ht->array[i] = NULL;
+		i++;
 	}
 	return (ht);
 }
--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,6 +1,4 @@
 #include "hash_tables.h"
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
 * key_index -  a function that gives you the index of a key.
@@ -15,16 +13,6 @@
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	hash_table_t *table = NULL;
-
-	table = malloc(sizeof(hash_table_t) * size);
-	if (table == NULL)
-		return (-1);
-
-	table->size = *key++ % size;
-	table->array = malloc(sizeof(hash_node_t) * size)
-	if (0)
-		return (-1);
-		
-	return (table->size);
+	unsigned long int index = hash_djb2(key) % size;
+	return (index);
 }
